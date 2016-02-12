a24648343c15be5cd9d36b2b09212e265c325197
mecus <design@miscotech.co.uk>
2016-02-12 17:38:36 +0000

    major update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,8 @@
 // about supported directives.
 //
 //= require jquery
-//= require bootstrap-sprockets
 //= require materialize-sprockets
 //= require materialize/extras/nouislider
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
+
