7b6f8b2d4e13d9c2b1535391c4320d385f74ae77
mecus <design@miscotech.co.uk>
2016-04-25 20:40:12 +0100

    advance styling

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160417185412) do
+ActiveRecord::Schema.define(version: 20160425162009) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
@@ -62,6 +62,17 @@ ActiveRecord::Schema.define(version: 20160417185412) do
     t.text     "message",    limit: 65535
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+  end
+
+  create_table "works", force: :cascade do |t|
+    t.string   "company",            limit: 255
+    t.string   "type",               limit: 255
+    t.string   "photo_file_name",    limit: 255
+    t.string   "photo_content_type", limit: 255
+    t.integer  "photo_file_size",    limit: 4
+    t.datetime "photo_updated_at"
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
   end
 
 end
